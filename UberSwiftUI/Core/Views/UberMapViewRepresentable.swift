6df7e21d7fe1867a36c38d52ae9377e6309a880a
sndwvv <songparkhk@gmail.com>
2022-10-05 20:10:47 +0700

    inital build with basic map view

---
+++ UberSwiftUI/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,43 @@
+//
+//  UberMapViewRepresentable.swift
+//  UberSwiftUI
+//
+//  Created by Songyee Park on 2022/10/05.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+	
+	let mapView = MKMapView()
+	
+	func makeUIView(context: Context) -> some UIView {
+		mapView.isRotateEnabled = false
+		mapView.showsUserLocation = true
+		mapView.userTrackingMode = .follow
+		return mapView
+	}
+	
+	func updateUIView(_ uiView: UIViewType, context: Context) {
+		
+	}
+	
+	func makeCoordinator() -> MapCoordinator {
+		return MapCoordinator(parent: self)
+	}
+	
+}
+
+extension UberMapViewRepresentable {
+	
+	class MapCoordinator: NSObject, MKMapViewDelegate {
+		let parent: UberMapViewRepresentable
+		
+		init(parent: UberMapViewRepresentable) {
+			self.parent = parent
+			super.init()
+		}
+	}
+	
+}
