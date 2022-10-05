4ce25b1e84f5f5f3c342683d95df445f61287e09
sndwvv <songparkhk@gmail.com>
2022-10-05 20:46:03 +0700

    updates map to user location

---
+++ UberSwiftUI/Managers/LocationManager.swift
@@ -0,0 +1,29 @@
+//
+//  LocationManager.swift
+//  UberSwiftUI
+//
+//  Created by Songyee Park on 2022/10/05.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+	
+	private let locationManager = CLLocationManager()
+	
+	override init() {
+		super.init()
+		locationManager.delegate = self 
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.startUpdatingLocation()
+	}
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard !locations.isEmpty else { return }
+		locationManager.stopUpdatingLocation()
+	}
+}
