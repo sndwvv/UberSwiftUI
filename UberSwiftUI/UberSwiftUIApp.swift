022c01fd9347ed2a38a4d61d424332ff3d8c78bb
sndwvv <songparkhk@gmail.com>
2022-10-05 19:24:14 +0700

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Songyee Park on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
