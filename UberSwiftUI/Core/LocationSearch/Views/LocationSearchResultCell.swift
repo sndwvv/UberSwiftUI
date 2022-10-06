00a7b90533c584015ebee042e85ac0167f1d48f6
sndwvv <songparkhk@gmail.com>
2022-10-06 19:21:41 +0700

    added location search skeleton ui

---
+++ UberSwiftUI/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -0,0 +1,38 @@
+//
+//  LocationSearchResultCell.swift
+//  UberSwiftUI
+//
+//  Created by Songyee Park on 2022/10/06.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+		HStack {
+			Image(systemName: "mappin.circle.fill")
+				.resizable()
+				.foregroundColor(.blue)
+				.accentColor(.white)
+				.frame(width: 40, height: 40)
+			
+			VStack(alignment: .leading) {
+				Text("Starbucks Coffee")
+					.font(.body)
+				Text("123 Main St, Cupertino CA")
+					.font(.system(size: 15))
+					.foregroundColor(.gray)
+				
+				Divider()
+			}
+			.padding([.leading, .vertical], 8)
+		}
+		.padding(.leading)
+    }
+}
+
+struct LocationSearchResultCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchResultCell()
+    }
+}
