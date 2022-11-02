b7bc9dd890159788663bd218147e5ff08d336ede
sndwvv <songparkhk@gmail.com>
2022-11-02 23:51:50 +0900

    added ride request view and asset files

---
+++ UberSwiftUI/Core/Trips/RideRequestView.swift
@@ -0,0 +1,104 @@
+//
+//  RideRequestView.swift
+//  UberSwiftUI
+//
+//  Created by Songyee Park on 2022/11/02.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+		VStack {
+			Capsule()
+				.foregroundColor(Color(.systemGray5))
+				.frame(width: 48, height: 6)
+			
+			HStack {
+				// Indicator View
+				VStack {
+					Circle()
+						.fill(Color(.systemGray3))
+						.frame(width: 8, height: 8)
+					
+					Rectangle()
+						.fill(Color(.systemGray3))
+						.frame(width: 1, height: 32)
+					
+					Rectangle()
+						.fill(Color(.black))
+						.frame(width: 8, height: 8)
+				}
+				
+				// Trip Info View
+				VStack(alignment: .leading, spacing: 24) {
+					HStack {
+						Text("Current location")
+							.font(.system(size: 16, weight: .semibold))
+							.foregroundColor(.gray)
+						
+						Spacer()
+						
+						Text("1:30 PM")
+							.font(.system(size: 14, weight: .semibold))
+							.foregroundColor(.gray)
+					}
+					.padding(.bottom, 10)
+					
+					HStack {
+						Text("Destination")
+							.font(.system(size: 16, weight: .semibold))
+						
+						Spacer()
+						
+						Text("1:45 PM")
+							.font(.system(size: 14, weight: .semibold))
+							.foregroundColor(.gray)
+					}
+				}
+				.padding(.leading, 8)
+			}
+			.padding()
+			
+			Divider()
+			
+			Text("SUGGESTED RIDES")
+				.font(.subheadline)
+				.fontWeight(.semibold)
+				.padding()
+				.foregroundColor(.gray)
+				.frame(maxWidth: .infinity, alignment: .leading)
+			
+			ScrollView(.horizontal) {
+				HStack(spacing: 12) {
+					ForEach(0 ..< 3, id: \.self) { _ in
+						VStack(alignment: .leading) {
+							Image("uber-x")
+								.resizable()
+								.scaledToFit()
+							
+							VStack(spacing: 4) {
+								Text("Uber X")
+									.font(.system(size: 14, weight: .semibold))
+								
+								Text("$22.04")
+									.font(.system(size: 14, weight: .semibold))
+							}
+							.padding(8)
+						}
+						.frame(width: 112, height: 140)
+						.background(Color(.systemGroupedBackground))
+						.cornerRadius(10)
+					}
+				}
+			}
+			.padding(.horizontal)
+		}
+    }
+}
+
+struct RideRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        RideRequestView()
+    }
+}
